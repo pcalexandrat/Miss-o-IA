a5c2acb2aa66eecbc170291f6108e3766518b581
Alexandra <127262337+pcalexandrat@users.noreply.github.com>
2024-08-14 19:18:19 -0300

    botões ok

--- script.js
+++ script.js
@@ -67,7 +67,7 @@ function mostraAlternativas(){
         const botaoAlternativas = document.createElement("button");
         botaoAlternativas.textContent = alternativa.texto;
         botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
-        caixaAlternativas.appendChild(botaoAlternativas)
+        caixaAlternativas.appendChild(botaoAlternativas);
     }
 }
 
