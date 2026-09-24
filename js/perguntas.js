527a911bb9e60c1e0318fc4ed0cab94358609510
Alexandra <127262337+pcalexandrat@users.noreply.github.com>
2026-09-24 09:38:01 -0300

    .

--- js/perguntas.js
+++ js/perguntas.js
@@ -4,15 +4,15 @@ export const perguntas = [
         alternativas: [
             {
                 texto: "Utilizando palestras para prevenir o cyberbullying nas escolas.", 
-                afirmacao:[ "Você é uma pessoa reflexiva e busca aprendizado e conhecimento.",
+                afirmacao: ["Você é uma pessoa reflexiva e busca aprendizado e conhecimento.",
                            "Apesar de buscar sempre conhecimento, você tem preocupação com o desconhecido"
-                ]
+                           ]
                         },
             {
                 texto:  "Prestando atenção aos sinais de cyberbullying nas escolas.",
-                afirmacao:[ "Você é uma pessoa observadora, pois se preocupa com o bem estar dos seus alunos.",
+                afirmacao: ["Você é uma pessoa observadora, pois se preocupa com o bem estar dos seus alunos.",
                            "Você se preocupa com o cyberbullying mas sabe de suas obrigações e tenta resolver as dificuldades"
-                ]
+                        ]
                         }    
            
         ]
@@ -23,9 +23,9 @@ export const perguntas = [
             alternativas: [
                 {
                     texto: "A energia geotérmica precisa ser mais explorada visto que utiliza o calor interno da terra, pois não depende do clima e é inesgotável.",
-                    afirmacao: ["Você demonstra um perfil  de pesquisador e inovador que busca por meio da inovação soluções para o meio ambiente e sustentabilidade das gerações posteriores.",
+                    afirmacao:["Você demonstra um perfil  de pesquisador e inovador que busca por meio da inovação soluções para o meio ambiente e sustentabilidade das gerações posteriores.",
                                "Você acredita que sempre haverá soluções construidas pelas novas gerações"
-                            ]
+                             ]
                             },
                 {
                     texto:  "Seria interessante explorar primeiramente fontes renováveis exotérmicas, em segundo plano utilizar a energia geotérmica apesar dos custos elevados de explorações e possibilidades de contaminação de rios. ",
